afb61a574b718ac187877b0d3d625e7e8981edb5
ume0117 <ume0117@gmail.com>
2026-06-19 15:10:47 +0900

    fix: アバター画像のCSP修正

--- next.config.ts
+++ next.config.ts
@@ -13,7 +13,7 @@ const nextConfig: NextConfig = {
               "script-src 'self' 'unsafe-eval' 'unsafe-inline'",
               "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com",
               "font-src 'self' https://fonts.gstatic.com",
-              "img-src 'self' data: https://cdn.bsky.app https://av-cdn.bsky.app",
+              "img-src 'self' data: https://cdn.bsky.app https://av-cdn.bsky.app https://*.bsky.app;
               "connect-src 'self' https://api.bsky.app https://bsky.social https://*.supabase.co",
               "worker-src 'self'",
             ].join("; "),
